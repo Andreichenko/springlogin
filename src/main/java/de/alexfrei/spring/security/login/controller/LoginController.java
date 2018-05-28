4cd9686bd995edb8f36159bf9a42ba890ffd1036
aleksandrandreichenko <rsn_alex@mail.ru>
2018-05-29 01:46:43 +0300

    Login Controller

---
+++ src/main/java/de/alexfrei/spring/security/login/controller/LoginController.java
@@ -0,0 +1,17 @@
+package de.alexfrei.spring.security.login.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String loginPage(Model model){
+        return "login";
+    }
+}
