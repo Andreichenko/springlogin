9c209b6b1848eadac0423ea110e6ed1dccb361ef
aleksandrandreichenko <rsn_alex@mail.ru>
2018-05-31 21:24:30 +0300

    fix

---
+++ src/main/java/de/alexfrei/spring/security/login/config/Initializer.java
@@ -0,0 +1,15 @@
+package de.alexfrei.spring.security.login.config;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class Initializer implements WebApplicationInitializer{
+
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+    }
+}
--- src/main/java/de/alexfrei/spring/security/login/servcie/UserDetailsServiceImpl.java
+++ src/main/java/de/alexfrei/spring/security/login/servcie/UserDetailsServiceImpl.java
@@ -1,12 +1,18 @@
 package de.alexfrei.spring.security.login.servcie;
 
 
+import de.alexfrei.spring.security.login.entity.enums.UserRoleEnums;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService{
@@ -17,7 +23,10 @@ public class UserDetailsServiceImpl implements UserDetailsService{
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userService.getUser("user");
-
-        return null;
+        Set<GrantedAuthority> roles = new HashSet();
+        roles.add(new SimpleGrantedAuthority(UserRoleEnums.USER.name()));
+        UserDetails userDetails =
+                new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), roles);
+        return userDetails;
     }
 }
