884e60df1e9185a92eca790a66b953d9434719e1
aleksandrandreichenko <rsn_alex@mail.ru>
2018-06-01 23:08:12 +0300

    fix

--- src/main/java/de/alexfrei/spring/security/login/config/Initializer.java
+++ src/main/java/de/alexfrei/spring/security/login/config/Initializer.java
@@ -5,6 +5,7 @@ import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.Registration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
